1f71c99cb939066bdb8fadc5493158a4aa54b7ba
MohamedReddam1 <2003103100139@ofppt-edu.ma>
2023-11-07 17:28:20 +0100

    first commit

---
+++ src/tp2_react/Image_data.js
@@ -0,0 +1,7 @@
+export const Image=[
+    {img:require("./imgs/luffyG5.jpeg")},
+    {img:require("./imgs/Yami.jpeg")},
+    {img:require("./imgs/GojoSaturo.jpeg")},
+    {img:require("./imgs/Sukuna.jpeg")},
+    {img:require("./imgs/Mikey.jpeg")}   
+]
